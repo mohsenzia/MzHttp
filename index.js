24b4a35c86987db710142fa29eea7c2bead36a9a
mohsenzia <mohsenzia91@gmail.com>
2023-01-15 13:17:06 +0330

    Add log mechanism with Log4JS

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+const log4js = require('log4js');
+log4js.configure({
+  appenders: { app: { type: "file", filename: "logs.log" } },
+  categories: { default: { appenders: ["app"], level: "info" } },
+});
+const logger = log4js.getLogger('app');
+
+
+app.use((req,res,next)=> {
+	logger.info(`[${req.method}]`, req.url, req.query);
+	next();
+})
+
 app.get('/', (req, res)=>{
 	res.status(200).send('Running...');
 });
