96c837e983e2eb7740d88dd6e1883958feba8891
mohsenzia <mohsenzia91@gmail.com>
2023-01-15 12:45:55 +0330

    Add Endpoints

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-	res.status(200).send('Hello');
+	res.status(200).send('Running...');
+});
+
+app.get('/hello', (req, res)=>{
+	if(req?.query?.name) {
+		res.status(200).send(`Hello ${req.query.name}`);
+	} else {
+		res.status(200).send('Hello Stranger');
+	}
+});
+
+app.get('/author', (req, res)=>{
+	res.status(200).send('Mohsen Ziaabadi');
 });
 
 app.listen(8080, ()=>{
